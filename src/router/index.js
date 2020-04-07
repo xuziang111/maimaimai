af3774764a29c400e7beab2d7ef522a59afbf618
xuziang111 <584788096@qq.com>
2020-04-07 23:44:49 +0800

    首页文件 别名设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+import index from '../pages/index/index.vue';
+import cart from '../pages/cart/cart.vue';
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+    path: '/index',
+    name: 'index',
+    component: index,
+    meta: { requiresAuth: false },
+},
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/cart',
+    name: 'cart',
+    component: cart,
+    meta: { requiresAuth: false },
+},
 ]
 
 const router = new VueRouter({
---
+++ vue.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    indexPath: 'index.html',
+    filenameHashing:true,
+
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'components': '@/components',
+                'css': '@/modules/css',
+                'js': '@/modules/js',
+                'views': '@/views',
+            }
+        }
+    },
+
+    pages: {
+        index: {
+            entry: "./src/pages/index/index.js", // 配置入口js文件
+            template: "./src/pages/index/index.html", // 主页面
+            filename: "index.html", // 打包后的html文件名称
+            title: "首页", // 打包后的.html中<title>标签的文本内容
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        cart: {//新增的部份
+            entry: "./src/pages/cart/cart.js", // 配置入口js文件
+            template: "./src/pages/cart/cart.html", // 主页面
+            filename: "cart.html", // 打包后的html文件名称
+            title: "首页", // 打包后的.html中<title>标签的文本内容
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'cart']
+        },        
+        },
+}
