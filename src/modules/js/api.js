fd649412db5915d2aeec6a4229d302c08645ee4a
xuziang111 <584788096@qq.com>
2020-04-08 11:13:26 +0800

    引入mint-ui 首页下拉加载

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -1,14 +1,15 @@
 import { forestgreen } from "color-name"
 
 let url ={
-    hotLists:'/index/hotLists'
+    hotLists:'/get/example/1586250292915'
 }
 
 
 //开发环境与生产环境的手动切换
 // let host = ''
 
-let host = 'http://rap2.taobao.org:38080/app/mock/7058/get'
+// let host = 'http://rap2.taobao.org:38080/app/mock/7058/get'
+let host = 'http://rap2.taobao.org:38080/app/mock/249892'
 
 for(let key in url){
     if(url.hasOwnProperty(key)){
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,20 +4,43 @@ import './index.css'
 import Vue from 'vue/dist/vue.esm.js'
 import axios from 'axios'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui';
 
+Vue.use(InfiniteScroll);
 Vue.prototype.axios = axios
 
 console.log(url)
-axios.post(url.hotLists,{
-    pageNum:1,
-    pageSize:6
-}).then(res=>{
-console.log(res)
-})
 
 new Vue({
     data:{
-        lists:null
+        lists:null,
+        pageNum:1,
+        loading:false,
+        allLoaded:false,
     },
+    methods:{
+        loadMore(){
+
+        },
+        getLists(){
+            this.loading = true
+            axios.post(url.hotLists,{
+                pageNum:this.pageNum,
+                pageSize:10
+            }).then(res=>{
+            console.log(res)
+            this.pageNum++
+            if(this.lists){
+                this.lists = this.lists.concat(res.data.lists)
+            }else{
+                this.lists = res.data.lists
+            }
+            this.loading = false
+            })
+        },
+    },
+    created(){
+        this.getLists()
+    }
     // render:h=>h(App)
 }).$mount("#app")
