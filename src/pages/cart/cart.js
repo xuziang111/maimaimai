4f26e2ded48aa9860b43aceced76552a53397200
xuziang111 <584788096@qq.com>
2020-04-13 10:06:44 +0800

    合并

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './cart.vue'
- 
+import url from 'js/api.js'
+import axios from "axios"
+import mixin from "js/mixin"
+
 import 'css/common.css'
 import './cart.css'
 import './cart_base.css'
@@ -8,5 +11,13 @@ import './cart_trade.css'
 Vue.config.productionTip = false
  
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  data:{
+
+  },
+  computed:{
+
+  },
+  sreated:{
+    
+  }
+}).$mount('.container')
--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
             entry: "./src/pages/cart/cart.js", // 配置入口js文件
             template: "./src/pages/cart/cart.html", // 主页面
             filename: "cart.html", // 打包后的html文件名称
-            title: "购物", // 打包后的.html中<title>标签的文本内容
+            title: "购物车", // 打包后的.html中<title>标签的文本内容
             // 在这个页面中包含的块，默认情况下会包含
             // 提取出来的通用 chunk 和 vendor chunk。
             chunks: ['chunk-vendors', 'chunk-common', 'cart']
